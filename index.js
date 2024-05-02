028f6960b370e3b8675a232a5df10c5edab1e27a
ssshlnv <144694774+ssshlnv@users.noreply.github.com>
2024-05-02 12:55:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1
+// если сначала нажать на 1, то 1, а потом 2, если сначала нажать 2, то 2
+
+// 2
+// const loadImageBtn = document.getElementById('loadImageBtn');
+// const imageContainer = document.getElementById('imageContainer');
+
+// loadImageBtn.addEventListener('click', () => {
+//     const imageUrl = 'https://cdn.iz.ru/sites/default/files/news-2024-01/1_4_4.jpg'; 
+
+//     const image = new Image();
+//     image.src = imageUrl;
+
+//     image.onload = () => {
+//         imageContainer.innerHTML = ''; 
+//         imageContainer.appendChild(image);
+//     };
+
+//     image.onerror = () => {
+//         imageContainer.innerHTML = 'Ошибка при загрузке картинки.';
+//     };
+// });
+
+// 3
+// нужно исправить
+// elem.addEventListener('click', function() {
+//     try {
+//         JSON.parse('some string');
+//     } catch(error) {
+//         console.log('Некорректный JSON:', error);
+//     }
+// });
+
